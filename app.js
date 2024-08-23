a766d74c43111a793662a244b00a90019ed21786
Maria Yajaira Montemira <202172019@uich.edu.mx>
2024-08-23 13:21:35 -0600

    Files structure completed

--- app.js
+++ app.js
@@ -2,25 +2,33 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var bodyParser = require('body-parser');
+var cssLoader = require('css-loader');
+var ejs = require('ejs');
+var axios = require('axios');
+var mysql = require('mysql2');
+var multer = require('multer');
+var fs = require('fs');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var get = require('./routes/get');
+var post = require('./routes/post');
+var api = require('./routes/api');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', get);
+app.use('/', post);
+app.use('/', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/post.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var router = express.Router();
+
+//Here must be a code functions
+
+module.exports = router;
