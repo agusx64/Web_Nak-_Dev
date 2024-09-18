6155c1c88215b55084088c35b1afec5f63f3c7dd
Agus <agusm1253@gmail.com>
2024-09-18 08:16:03 -0600

    Co-authored-by: MichiAndy30 <MichiAndy30@users.noreply.github.com>

--- routes/get.js
+++ routes/get.js
@@ -2,12 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
 
-    res.render('index', { title: 'Express' });
+    res.render('index');
 
 });
 
 //Here must be a code functions
+
+router.get('/shop_page', function(req, res) {
+
+    res.render('shop');
+
+})
+
+module.exports = router;
+
+router.get('/cambios_admin', function(req, res) {
+
+    res.render('admin');
+
+})
 
 module.exports = router;
