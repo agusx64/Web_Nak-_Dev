d12c2c610f468ea489da78c1b997660b4cfcf22b
Agus <agusm1253@gmail.com>
2025-01-10 09:28:34 -0600

    Nombre de la base de datos corregido

--- routes/post.js
+++ routes/post.js
@@ -24,7 +24,7 @@ let connection = mysql.createConnection({
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE 
 });
 
 connection.connect(function(err) {
